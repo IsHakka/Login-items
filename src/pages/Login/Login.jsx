d79adc4a5354c6d26b70b570e866028bb8c35a9f
Xian <qaq81704@gmail.com>
2023-10-22 22:11:54 +0800

    file📦: request封裝

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,29 +1,41 @@
 import './index.scss';
 import { Card, Form, Input, Button } from 'antd';
-import logo from './../../assets/logo.png';
+import logo from './../../assets/logo.jpeg';
 
 const Login = () => {
-  return (
-    <div className="login">
-      <Card className="login-container">
-        <img className="login-logo" src={logo} alt="" />
-        {/* 登录表单 */}
-        <Form>
-          <Form.Item>
-            <Input size="large" placeholder="请输入手机号" />
-          </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" size="large" block>
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
-  )
+    const onFinish = (values) => {
+        console.log('Success:', values);
+      };
+    return (
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form onFinish={onFinish} validateTrigger="onBlur">
+                    <Form.Item
+                        name="mobile"
+                        // 多層驗證
+                        rules={[{ required: true, message: '請輸入手機號碼' },
+                        {
+                            pattern: /^09\d{2}-?\d{3}-?\d{3}$/,
+                            message: '請輸入正確的手機格式'
+                        }]}>
+                        <Input size="large" placeholder="請輸入手機號碼" />
+                    </Form.Item>
+                    <Form.Item
+                        name="code"
+                        rules={[{ required: true, message: '請輸入驗證碼' }]}>
+                        <Input size="large" placeholder="請輸入驗證碼" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登入
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 
 export default Login
---
+++ src/utils/request.jsx
@@ -0,0 +1,33 @@
+// 封裝處理
+import axios from 'axios';
+// 根域名配置
+// 超時時間
+// 添加請求攔截器 響應攔截器
+
+const request = axios.create({
+    baseURL: 'http://geek.itheima.net/v1_0',
+    // 超時時間
+    timeout: 5000
+})
+
+// 添加請求攔截器
+// 在請求發送前做配置[參數處理]
+request.interceptors.request.use((config) => {
+    return config
+}, (error) => {
+    return Promise.reject(error)
+})
+
+// 添加響應攔截器
+// 在響應返回到客戶端之前做攔截，重點處理返回的數據
+request.interceptors.response.use((response) => {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response.data
+}, (error) => {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error)
+})
+
+export { request };

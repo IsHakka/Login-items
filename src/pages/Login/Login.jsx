77b7a62cb7ec5ad6e772aad9888ddc054f6b3ea1
Xian <qaq81704@gmail.com>
2023-10-22 14:56:57 +0800

    file📦: 新增assets

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import './index.scss';
+import { Card, Form, Input, Button } from 'antd';
+import logo from './../../assets/logo.png';
 
 const Login = () => {
-    return (
-        <div>
-            login
-        </div>
-    );
-};
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
+}
 
-export default Login;
+export default Login

bb41455ce560766ad69d8cab4c8b76c796893591
Xian <qaq81704@gmail.com>
2023-10-23 12:35:07 +0800

    feat✨: 圖表初始化

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
 
-const Home = () => {
-    return (
-        <div>
-            1
-        </div>
-    );
-};
 
-export default Home;
+const Home = ({ title }) => {
+  const chartRef = useRef(null)
+  useEffect(() => {
+
+    const chartDom = chartRef.current
+
+    const myChart = echarts.init(chartDom)
+
+    const option = {
+      title: {
+        text: title
+      },
+      xAxis: {
+        type: 'category',
+        data: ['國中生', '高中生', '大學生']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    }
+  
+    option && myChart.setOption(option)
+
+  }, [title])
+  return <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+}
+
+export default Home

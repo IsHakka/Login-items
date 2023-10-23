64f359c4f3ac6d66808fe64e9bd745d1e7a754f9
Xian <qaq81704@gmail.com>
2023-10-23 13:38:07 +0800

    style: 文章結構搭建

--- src/pages/Publish/Publish.jsx
+++ src/pages/Publish/Publish.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import {
+    Card,
+    Breadcrumb,
+    Form,
+    Button,
+    Radio,
+    Input,
+    Upload,
+    Space,
+    Select
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const { Option } = Select
 
 const Publish = () => {
     return (
-        <div>
-            3
-        </div>
-    );
-};
+        <div className="publish">
+            <Card
+                title={
+                    <Breadcrumb items={[
+                        { title: <Link to={'/'}>首頁</Link> },
+                        { title: '發布文章' },
+                    ]}
+                    />
+                }
+            >
+                <Form
+                    labelCol={{ span: 4 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ type: 1 }}
+                >
+                    <Form.Item
+                        label="標題"
+                        name="title"
+                        rules={[{ required: true, message: '請輸入文章標題' }]}
+                    >
+                        <Input placeholder="請輸入文章標題" style={{ width: 400 }} />
+                    </Form.Item>
+                    <Form.Item
+                        label="頻道"
+                        name="channel_id"
+                        rules={[{ required: true, message: '請選擇文章頻道' }]}
+                    >
+                        <Select placeholder="請選擇文章頻道" style={{ width: 400 }}>
+                            <Option value={0}>推薦</Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label="内容"
+                        name="content"
+                        rules={[{ required: true, message: '請輸入文章內容' }]}
+                    ><textarea name="" id="" cols="30" rows="10"></textarea></Form.Item>
 
-export default Publish;
+                    <Form.Item wrapperCol={{ offset: 4 }}>
+                        <Space>
+                            <Button size="large" type="primary" htmlType="submit">
+                                馬上分佈
+                            </Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
+}
+
+export default Publish

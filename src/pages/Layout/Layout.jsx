2a684f18be7cd365164b6b8ba26426fdfecc4fee
Xian <qaq81704@gmail.com>
2023-10-23 11:56:56 +0800

    feat✨: 退出登入

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,42 +1,69 @@
-import { Layout, Menu, Popconfirm } from 'antd'
+import { Layout, Menu, Popconfirm } from 'antd';
 import {
   HomeOutlined,
   DiffOutlined,
   EditOutlined,
   LogoutOutlined,
-} from '@ant-design/icons'
-import './index.scss'
-import { Outlet } from 'react-router-dom'
+} from '@ant-design/icons';
+import './index.scss';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserInfo, clearUserInfo } from '../../store/modules/user';
+
 
 const { Header, Sider } = Layout
 
 const items = [
   {
     label: '首頁',
-    key: '1',
+    key: '/',
     icon: <HomeOutlined />,
   },
   {
     label: '文章管理',
-    key: '2',
+    key: '/article',
     icon: <DiffOutlined />,
   },
   {
-    label: '创建文章',
-    key: '3',
+    label: '創建文章',
+    key: '/publish',
     icon: <EditOutlined />,
   },
+
 ]
 
 const GeekLayout = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onMenuClick = (router) => {
+    const path = router.key;
+    navigate(path)
+  } 
+  // 獲取當前路徑
+  const location = useLocation()
+  const selectKey = location.pathname
+  // 觸發個人用戶信息
+  useEffect(() => {
+    dispatch(fetchUserInfo())
+  }, [dispatch])
+
+  const { userInfo } = useSelector(state => state.user)
+  // 登出，清除信息
+  const onConfirm = () => {
+    dispatch(clearUserInfo())
+    navigate('login')
+  }
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
         <div className="user-info">
-          <span className="user-name">Xian</span>
+          <span className="user-name">{userInfo.name}</span>
           <span className="user-logout">
-            <Popconfirm title="是否確認退出？" okText="退出" cancelText="取消">
+            <Popconfirm title="是否確認退出？" okText="退出" cancelText="取消" onConfirm={onConfirm}>
               <LogoutOutlined /> 退出
             </Popconfirm>
           </span>
@@ -47,11 +74,13 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={['1']}
+            selectedKeys={selectKey}
+            onClick={onMenuClick}
             items={items}
             style={{ height: '100%', borderRight: 0 }}></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
+          {/* 內容 */}
           <Outlet></Outlet>
         </Layout>
       </Layout>
--- src/store/modules/user.jsx
+++ src/store/modules/user.jsx
@@ -1,13 +1,16 @@
 // 用戶相關狀態管理
 import { createSlice } from '@reduxjs/toolkit';
 import { request } from '../../utils/request';
-import { setToken as _setToken, getToken } from '../../utils/tokem';
+import { setToken as _setToken, getToken, removeToken } from '../../utils/tokem';
 
 export const userStore = createSlice({
     name: 'user',
     // 數據狀態
     initialState: {
-        token: getToken() || ''
+        token: getToken() || '',
+        userInfo: {
+
+        }
     },
     // 同步修改方法
     reducers: {
@@ -15,11 +18,19 @@ export const userStore = createSlice({
             state.token = action.payload;
             // 存儲本地
             _setToken(action.payload)
+        },
+        setUserInfo(state, action) {
+            state.userInfo = action.payload;
+        },
+        clearUserInfo(state) {
+            state.token = ''
+            state.userInfo = {}
+            removeToken()
         }
     }
 });
 
-// 異步獲取token
+// 登入:異步獲取token
 export const fetchLogin = (loginForm) => {
     return async (dispatch) => {
         // 發送異步請求
@@ -28,6 +39,13 @@ export const fetchLogin = (loginForm) => {
         dispatch(setToken(res.data.token));
     }
 }
+// 獲取個人用戶訊息異步方法
+export const fetchUserInfo = () => {
+    return async (dispatch) => {
+        const res = await request.get('/user/profile')
+        dispatch(setUserInfo(res.data))
+    }
+}
 
-export const { setToken } = userStore.actions;
+export const { setToken, setUserInfo, clearUserInfo } = userStore.actions;
 
